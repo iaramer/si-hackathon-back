b79969cb1e91103f220340ae638904d108a26d13
Ilia_Vertiletskii <Ilia_vertiletskii@epam.com>
2019-10-04 20:04:02 +0300

    spring skeleton

---
+++ src/main/java/com/epam/examreviewer/ExamReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.examreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamReviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/examreviewer/ExamReviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.examreviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamReviewerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
