65dd853c46ca00a659abc2a2dadf25764b6b6be9
Daniil_Shakurov <daniil_shakurov@epam.com>
2019-10-05 00:15:59 +0300

    Fix LanguagesController

--- src/main/java/com/epam/examreviewer/controller/LanguagesController.java
+++ src/main/java/com/epam/examreviewer/controller/LanguagesController.java
@@ -5,32 +5,28 @@ import com.epam.examreviewer.model.Language;
 import com.epam.examreviewer.model.Topic;
 import com.epam.examreviewer.service.LanguageService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/languages")
+@RequiredArgsConstructor
 public class LanguagesController {
 
   private final LanguageService languageService;
 
-  public LanguagesController(LanguageService languageService) {
-    this.languageService = languageService;
-  }
-
-  @GetMapping(value = "/all_languages")
+  @GetMapping(value = "/languages")
   @ResponseStatus(HttpStatus.OK)
-  public List<Language> getAllLanguages(@PathVariable Exam examId) {
+  public List<Language> getAllLanguages(@RequestParam Exam examId) {
     return languageService.getAllLanguages(examId);
   }
 
-  @GetMapping(value = "/all_topics")
+  @GetMapping(value = "/topics")
   @ResponseStatus(HttpStatus.OK)
-  public List<Topic> getAllTopics(@PathVariable Exam examId, @PathVariable Language langId) {
+  public List<Topic> getAllTopics(@RequestParam Exam examId, @RequestParam Language langId) {
     return languageService.getAllTopics(examId, langId);
   }
 }
