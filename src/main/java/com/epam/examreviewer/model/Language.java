5e85b1decb5edda012f054d74390927ed1a1d298
Daniil_Shakurov <daniil_shakurov@epam.com>
2019-10-05 01:01:11 +0300

    Fix LanguagesController

--- src/main/java/com/epam/examreviewer/model/Language.java
+++ src/main/java/com/epam/examreviewer/model/Language.java
@@ -1,11 +1,15 @@
 package com.epam.examreviewer.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document("languages")
 public class Language {
+
+  @Id
+  private Long id;
 
   private LanguageType languageType;
 
